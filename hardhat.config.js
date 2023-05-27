73b378cba4e2c9755473d08f97284de8690e51f8
Sheetal Ojha <sheetalojhabkn@gmail.com>
2023-05-28 03:25:06 +0530

    set deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,6 +24,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   networks: {
+
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Shakti = await hre.ethers.getContractFactory("Shakti");
+  const shakti = await Shakti.deploy();
 
-  await greeter.deployed();
+  await shakti.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("shakti deployed to:", shakti.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
